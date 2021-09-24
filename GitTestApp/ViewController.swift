2be6ff0809e499716ad1c38113d61a05e4b03a97
Onuralp <xonuraralpx@gmail.com>
2021-09-24 16:21:49 +0300

    Second Change

--- GitTestApp/ViewController.swift
+++ GitTestApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("varan1")
+        print("varan2")
     }
 
 
